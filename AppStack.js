f6d4e48328226e0e94eb3fdfcd351cb0c21d3fd3
belenbarrionuevo <82065693+belenbarrionuevo@users.noreply.github.com>
2022-10-13 00:04:50 -0300

    Add files via upload

---
+++ AppStack.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import Pantalla1 from './Screens/Pantalla1';
+import Pantalla2 from './Screens/Pantalla2';
+
+const Stack = createNativeStackNavigator();
+
+const AppStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Pantalla1}/>
+        <Stack.Screen name="Screen2" component={Pantalla2}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppStack;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
